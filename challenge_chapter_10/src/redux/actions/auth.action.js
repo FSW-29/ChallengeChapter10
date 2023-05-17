00bf6a07f7e66e4c80832b7318c02fb208209ec2
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-17 15:51:09 +0700

    buat action untuk login

--- challenge_chapter_10/src/redux/actions/auth.action.js
+++ challenge_chapter_10/src/redux/actions/auth.action.js
@@ -4,6 +4,7 @@ import axios from "axios";
 // => akan dikirim kedalam reducer dalam case ini 
 // => ./src/redux/reducers/auth/auth.reducer.js
 export const REGISTER_USER = "REGISTER_USER";
+export const LOGIN_USER = "LOGIN_USER";
 
 export const registerUser = (data) => {
   return async (dispatch) => {
@@ -43,4 +44,43 @@ export const registerUser = (data) => {
     }
   };
 }
+
+export const loginUser = (data) => {
+  return async (dispatch) => {
+    // > kondisi pending
+    dispatch({
+      type: LOGIN_USER,
+      payload: {
+        loading: true,
+        data: false,
+        errorMessage: false,
+      },
+    });
+
+    // > kondisi fulfilled
+    try {
+      const response = await axios.post('http://localhost:3000/api/auth/login', data);
+
+      dispatch({
+        type: LOGIN_USER,
+        payload: {
+          loading: false,
+          data: response.data,
+          errorMessage: false,
+        },
+      });
+    }
+    // > kondisi rejected 
+    catch (error) {
+      dispatch({
+        type: LOGIN_USER,
+        payload: {
+          loading: false,
+          data: false,
+          errorMessage: error.message,
+        },
+      });
+    }
+  };
+};
 
