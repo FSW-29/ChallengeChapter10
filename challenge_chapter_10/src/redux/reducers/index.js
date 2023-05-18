7094aed271e0da539c5778e4bf474b77bf4eb651
Steven Aprullianto <stevenaprullianto@gmail.com>
2023-05-18 11:58:13 +0700

    fix reducer

--- challenge_chapter_10/src/redux/reducers/index.js
+++ challenge_chapter_10/src/redux/reducers/index.js
@@ -1,17 +1,11 @@
 import { combineReducers } from "redux";
-import  usersLogin from "./profile/usersLogin_reducers";
+import usersLogin from "./profile/usersLogin_reducers";
 import authReducer from "./auth/auth.reducer";
 import gameReducer from "./game/game.reducer";
 
-
 export default combineReducers({
-<<<<<<< HEAD
-    authReducer,
-    usersLogin
-    //masukkan semua reducer ke sini
-})
-=======
   authReducer,
+  usersLogin,
   gameReducer,
+  //masukkan semua reducer ke sini
 });
->>>>>>> abcb1e2ace82df900218c91dcb87ccd7f3615305
