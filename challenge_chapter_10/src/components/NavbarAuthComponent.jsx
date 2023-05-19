8f40409ff835dd09ffe052914c6da56646611809
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-19 11:29:06 +0700

    Edit navbar

--- challenge_chapter_10/src/components/NavbarAuthComponent.jsx
+++ challenge_chapter_10/src/components/NavbarAuthComponent.jsx
@@ -1,32 +1,23 @@
+import Link from "next/link";
+
 const NavbarAuthComponent = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand" href="#">
+          <Link className="navbar-brand" href={"/"}>
             <img srcSet={"https://w7.pngwing.com/pngs/87/586/png-transparent-next-js-hd-logo.png"} alt="" width="50" height="50" />
-          </a>
+          </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" aria-current="page" href="#" style={{ fontSize: '18px' }}>Home</a>
+                <Link className="nav-link" aria-current="page" href={"/home"} style={{ fontSize: '18px' }}>Home</Link>
               </li> 
               <li className="nav-item">
-                <a className="nav-link" href="#" style={{ fontSize: '18px' }}>List Game</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ fontSize: '18px' }}>
-                  Category Game
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><hr className="dropdown-divider" /></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
+                <Link className="nav-link" href={"/GameList"} style={{ fontSize: '18px' }}>Game List</Link>
               </li>
             </ul>
           </div>
