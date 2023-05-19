a47c8ec9bc71181a804bad4bc257306acc7bf667
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-19 14:02:59 +0700

    setup dotenv (tapi belum berhasil)

--- challenge_chapter_10/src/services/firebase.js
+++ challenge_chapter_10/src/services/firebase.js
@@ -3,6 +3,8 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getDatabase } from 'firebase/database'
+// import * as dotenv from 'dotenv';
+// dotenv.config()
 
 const firebaseConfig = {
   apiKey: "AIzaSyBUh8nSDGnRWPqeiMentCtJU1l_f5ckPWo",
@@ -12,6 +14,13 @@ const firebaseConfig = {
   storageBucket: "binar-fsw-29.appspot.com",
   messagingSenderId: "869942278872",
   appId: "1:869942278872:web:33273fba01a1a024dcf5ad"
+  // apiKey: process.env.FIREBASE_API_KEY,
+  // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.FIREBASE_DATABASE_URL,
+  // projectId: process.env.FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
